e8ba72f28a7e06105b3f4dd9e2e6109a294e55e6
Tudor Iovita <iovitatudor21@gmail.com>
2022-11-11 15:57:55 +0200

    Fix api view page

--- resources/views/front/api.blade.php
+++ resources/views/front/api.blade.php
@@ -104,6 +104,3 @@
     </main>
 
 @stop
-
-
-<iframe width=\"100%\" height=\"315\" src=\"https://www.youtube.com/embed/2fknwD3NJLU\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>
